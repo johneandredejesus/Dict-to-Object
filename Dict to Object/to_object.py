461f731d8231d43911047f0ff62c8f5a423b23be
johne <johne@localhost.localdomain>
2020-07-23 20:22:20 -0300

    added is_iterable

--- Dict to Object/to_object.py
+++ Dict to Object/to_object.py
@@ -1,5 +1,14 @@
 from collections import OrderedDict
 
+class List(list):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    def is_iterable(self):
+        return True
+
+        
 class  Object(object):
     '''To convert dict recursively'''
     def __init__(self, value):
@@ -8,13 +17,16 @@ class  Object(object):
             self.__dict__ = self.__to_format_dict(value)
         else:
             self.__dict__ = { }
-        
+    
+    def is_iterable(self):
+        return False
+    
     def __to_format_dict(self, dictvalue):
         return { self.__to_format_key(key) : self.__to_get_item_to_convert_to_object(value) for key, value in dictvalue.items() }
          
     def __to_get_item_to_convert_to_object(self, value):
-        if type(value) is list:
-            return [ self.__to_get_item_to_convert_to_object(obj) for obj in value ]
+        if type(value) is list or type(value) is tuple or type(value) is set:
+            return List( self.__to_get_item_to_convert_to_object(obj) for obj in value )
         elif type(value) is OrderedDict:
             return Object(value)
         elif type(value) is dict:
@@ -27,3 +39,4 @@ class  Object(object):
             return key.replace(' ', '_')
         else:
             return key.replace('-', '_')
+
